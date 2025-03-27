3ac43040ab6dc6e9030198d41c6e97e31adfb723
kmin1231 <kmin91143@gmail.com>
2025-03-28 02:20:18 +0900

    🌱 feat: #1 enable Swagger UI for API documentation

--- main.go
+++ main.go
@@ -5,23 +5,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	_ "github.com/GDG-on-Campus-KHU/SDGP_team5_BE/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
+
+// @title SDGP-team5-BE
+// @version 1.0
+// @description API documentation
+// @host localhost:5000
+// @BasePath /
+
+// @Summary check server status
+// @Description checks if the server is running
+// @Tags Status
+// @Accept json
+// @Produce json
+// @Success 200 {string} string "Server is running"
+// @Router / [get]
+func handler(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("Server is running at http://%s:%s", host, port)
+	fmt.Fprintf(w, "%s\n", message)
+}
 
 var (
 	host = "localhost"
 	port = "5000"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("Server is running at http://%s:%s", host, port)
-	fmt.Fprintf(w, "%s\n", message)
-}
-
 func main() {
 	http.HandleFunc("/", handler)
 
+	http.Handle("/swagger/", httpSwagger.WrapHandler)
+
 	serverAddress := fmt.Sprintf("%s:%s", host, port)
 	fmt.Printf("Server is running at http://%s\n", serverAddress)
+	fmt.Printf("Swagger UI available at http://%s/swagger\n", serverAddress)
 
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Println("Error starting server:", err)
