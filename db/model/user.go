89df7f941e93f3f6cc803a8d32f83d98249ef18d
kmin1231 <kmin91143@gmail.com>
2025-04-25 20:10:42 +0900

    ✨ feat(db): #11 add 'Counter' schema for auto-incrementing document IDs

--- db/model/user.go
+++ db/model/user.go
@@ -7,15 +7,15 @@ import (
 )
 
 type User struct {
-	ID      primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"` // MongoDB ObjectId
-	UserID  int                `bson:"user_id" json:"user_id"`            // auto-increment integer
+	ID      primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`	// MongoDB ObjectId
+	UserID  int                `bson:"user_id" json:"user_id"`				// auto-increment integer
 	Name    string             `bson:"name" json:"name"`
 	Email   string             `bson:"email" json:"email"`
-	AppLang string             `bson:"app_lang" json:"app_lang"` // app 사용 언어 (ex. "ko", "en")
+	AppLang string             `bson:"app_lang" json:"app_lang"`			// app 사용 언어 (ex. "ko", "en")
 
-	CountryCode string `bson:"country_code" json:"country_code"` // 여행 중인 국가 코드 (ex. "KR", "US")
-	InfoID      int    `bson:"info_id" json:"info_id"`           // ref. struct 'MedicalInfo'
+	CountryCode string `bson:"country_code" json:"country_code"`			// 여행 중인 국가 코드 (ex. "KR", "US")
+	InfoID      int    `bson:"info_id" json:"info_id"`						// ref. struct 'MedicalInfo'
 
 	GroupIDs  []int `bson:"group_ids" json:"group_ids"`
-	Favorites []int `bson:"favorites" json:"favorites"` // situation index
+	Favorites []int `bson:"favorites" json:"favorites"`						// situation index
 }
