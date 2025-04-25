93995ff9cd2b1cbd6f88cc856bcabacdd8a92a90
kmin1231 <kmin91143@gmail.com>
2025-04-25 18:33:37 +0900

    ✨ feat(auth): #5 update MongoDB configuration

--- db/mongo.go
+++ db/mongo.go
@@ -15,6 +15,8 @@ import (
 
 var Client *mongo.Client
 
+var UserCollection *mongo.Collection
+
 func InitMongo() {
 	uri := os.Getenv("MONGO_URI")
 
@@ -38,4 +40,6 @@ func InitMongo() {
 
 	fmt.Println("Successfully connected to MongoDB Atlas.")
 	Client = client
+
+	UserCollection = Client.Database("resq").Collection("users")
 }
