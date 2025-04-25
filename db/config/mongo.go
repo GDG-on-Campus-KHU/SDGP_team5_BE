ad0802be6c213d074ec0e3ac30e08479993ae23b
kmin1231 <kmin91143@gmail.com>
2025-04-26 01:42:28 +0900

    ✨ feat(db): #11 initialize MongoDB collections

--- db/config/mongo.go
+++ db/config/mongo.go
@@ -18,6 +18,7 @@ var Client *mongo.Client
 var UserCollection *mongo.Collection
 var CountryCollection *mongo.Collection
 var RecordingCollection *mongo.Collection
+var GroupCollection *mongo.Collection
 
 func InitMongo() {
 	uri := os.Getenv("MONGO_URI")
@@ -48,4 +49,5 @@ func InitMongo() {
 	UserCollection = Client.Database("resq").Collection("users")
 	CountryCollection = Client.Database("resq").Collection("countries")
 	RecordingCollection = Client.Database("resq").Collection("recordings")
+	GroupCollection = Client.Database("resq").Collection("groups")
 }
