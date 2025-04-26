e3c46d90c87366d54d6357bb6124d5274cf47438
kmin1231 <kmin91143@gmail.com>
2025-04-26 19:27:47 +0900

    ✨ feat(group): #11 update 'Group' schema

--- db/model/group.go
+++ db/model/group.go
@@ -6,9 +6,24 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type GroupMember struct {
+	UserID    int    `bson:"user_id" json:"user_id"`
+	Email     string `bson:"email" json:"email"`
+	Status    string `bson:"status" json:"status"`
+	InvitedBy int    `bson:"invited_by" json:"invited_by"`
+}
+
+
 type Group struct {
 	ID			primitive.ObjectID	`bson:"_id,omitempty" json:"id,omitempty"`                 // MongoDB ObjectId
 	GroupName	string				`bson:"group_name" json:"group_name"`
-	Members		[]int				`bson:"members" json:"members"`								// ref. struct 'User'
+	Members		[]GroupMember		`bson:"members" json:"members"`
 	CreatedAt   primitive.DateTime 	`bson:"created_at,omitempty" json:"created_at,omitempty"`	// timestamp
+}
+
+
+type GroupMembersResponse struct {
+	GroupID primitive.ObjectID `json:"group_id"`
+	Name    string             `json:"name"`
+	Members []GroupMember      `json:"members"`
 }
