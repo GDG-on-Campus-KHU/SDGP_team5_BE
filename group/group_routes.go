68e7bc389849404b42481477bc56ad9cf5552316
kmin1231 <kmin91143@gmail.com>
2025-04-26 23:21:46 +0900

    ✨ feat(group): #10 update routes for 'group' module

--- group/group_routes.go
+++ group/group_routes.go
@@ -12,7 +12,7 @@ func RegisterGroupRoutes(r *gin.Engine, groupHandler *GroupHandler) {
 
 	// group routes
 	r.POST("/api/groups", auth.JWTAuthMiddleware(), groupHandler.CreateGroup)
-	r.GET("/api/groups/:id", auth.JWTAuthMiddleware(), groupHandler.GetGroupByID)
+	r.GET("/api/groups/:id", auth.JWTAuthMiddleware(), groupHandler.GetGroupByGroupID)
 	r.PUT("/api/groups/:id", auth.JWTAuthMiddleware(), groupHandler.UpdateGroup)
 	r.DELETE("/api/groups/:id", auth.JWTAuthMiddleware(), groupHandler.DeleteGroup)
 	r.GET("/api/groups", auth.JWTAuthMiddleware(), groupHandler.ListGroups)
