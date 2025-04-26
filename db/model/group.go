3f0e671dd4778ccf72e0912d8e745d2a0a38b4a2
kmin1231 <kmin91143@gmail.com>
2025-04-26 18:39:35 +0900

    ✨ feat(group): #11 update 'Group' schema (delete 'group_id')

--- db/model/group.go
+++ db/model/group.go
@@ -8,7 +8,6 @@ import (
 
 type Group struct {
 	ID			primitive.ObjectID	`bson:"_id,omitempty" json:"id,omitempty"`                 // MongoDB ObjectId
-	GroupID		int      			`bson:"group_id" json:"group_id"`
 	GroupName	string				`bson:"group_name" json:"group_name"`
 	Members		[]int				`bson:"members" json:"members"`								// ref. struct 'User'
 	CreatedAt   primitive.DateTime 	`bson:"created_at,omitempty" json:"created_at,omitempty"`	// timestamp
