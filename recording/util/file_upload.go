30d6829be6fc568271842607cad2675a4b07d938
kmin1231 <kmin91143@gmail.com>
2025-04-27 04:51:01 +0900

    ✨ feat(recording): #9 add function to file to GCS bucket

---
+++ recording/util/file_upload.go
@@ -0,0 +1,54 @@
+// recording/util/file_upload.go
+
+package util
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/storage"
+)
+
+// GCS bucket에 음성 녹음 파일 업로드하고 file URL을 return
+func UploadFileToGCS(file *multipart.FileHeader, userID string) (string, error) {
+	// GCS client
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	// GCS bucket
+	bucketName := "resq-upload-bucket"
+	bucket := client.Bucket(bucketName)
+
+	// unique filename generation rule
+	ext := strings.ToLower(file.Filename[strings.LastIndex(file.Filename, "."):])
+	timestamp := time.Now().Format("060102150405")	// yyMMddhhmmss
+	fileName := fmt.Sprintf("user_%s_%s%s", userID, timestamp, ext)
+
+	srcFile, err := file.Open()
+	if err != nil {
+		return "", fmt.Errorf("failed to open file: %v", err)
+	}
+	defer srcFile.Close()
+
+	// file upload
+	dst := bucket.Object(fileName).NewWriter(ctx)
+	dst.ContentType = file.Header.Get("Content-Type")
+	if _, err := io.Copy(dst, srcFile); err != nil {
+		return "", fmt.Errorf("failed to copy file to GCS: %v", err)
+	}
+	if err := dst.Close(); err != nil {
+		return "", fmt.Errorf("failed to close GCS writer: %v", err)
+	}
+
+	// return GCS file URL
+	url := fmt.Sprintf("https://storage.googleapis.com/%s/%s", bucketName, fileName)
+	return url, nil
+}
