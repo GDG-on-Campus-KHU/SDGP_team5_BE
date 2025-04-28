ad16eadf1200da5b6434d445f84bcc97034cdfa6
kmin1231 <kmin91143@gmail.com>
2025-04-29 03:43:18 +0900

    🐛 fix(recording): #9 update 'Recording' schema (SpeechToText language)

--- db/model/recording.go
+++ db/model/recording.go
@@ -14,6 +14,6 @@ type Recording struct {
 	UserID        int                	`bson:"user_id" json:"user_id"`                            // ref. struct 'User'
 	RecordingURL  string             	`bson:"recording_url" json:"recording_url"`                // 음성 파일 GCS URL
 	RecordingText string             	`bson:"recording_text,omitempty" json:"recording_text"`    // SpeechToText 결과물 (NULL by default)
-	CountryCode   string             	`bson:"country_code" json:"country_code"`                  // ref. struct ''User', 'Country'
+	AppLang       string             	`bson:"app_lang" json:"app_lang"`  
 	CreatedAt     time.Time			 	`bson:"created_at,omitempty" json:"created_at,omitempty"`	// timestamp
 }
