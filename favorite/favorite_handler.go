296de0305e715d6d41cfaa258015265cb5a1d1fe
SangYeong Park <tkddud386@gmail.com>
2025-05-02 01:57:31 +0900

    ✨feat: #22 implement addFavorite api endpoint & add userRepository

---
+++ favorite/favorite_handler.go
@@ -0,0 +1,61 @@
+// favorite/favorite_handler.go
+
+package favorite
+
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/GDG-on-Campus-KHU/SDGP_team5_BE/util"
+)
+
+type FavoriteHandler struct {
+	service *FavoriteService
+}
+
+func NewFavoriteHandler(service *FavoriteService) *FavoriteHandler {
+	return &FavoriteHandler{service: service}
+}
+
+// POST /api/favorites/:id
+// @Summary Add a new favorite
+// @Description Add a favorite using its ID
+// @Tags favorites
+// @Accept json
+// @Produce json
+// @Param id path string true "Situation ID"
+// @Success 200 {object} map[string]string "Favorite added successfully"
+// @Failure 400 {object} map[string]string "Invalid situation ID or user ID"
+// @Failure 401 {object} map[string]string "Unauthorized access"
+// @Failure 500 {object} map[string]string "Internal server error"
+// @Router /api/favorites/{id} [post]
+func (h *FavoriteHandler) AddFavorite(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	situationIndexStr := c.Param("index")
+	situationIndex, err := strconv.Atoi(situationIndexStr)
+	if err != nil {
+		util.RespondBadRequest(c, "Invalid situation ID")
+		return
+	}
+
+	userIDStr, err := util.GetUserIDFromContext(c)
+	if err != nil {
+		util.RespondUnauthorized(c, "Unauthorized access")
+		return
+	}
+
+	userId, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		util.RespondBadRequest(c, "Invalid user ID")
+		return
+	}
+
+	if err := h.service.AddFavorite(ctx, userId, situationIndex); err != nil {
+		util.RespondInternalError(c, err.Error())
+		return
+	}
+
+	util.RespondSuccess(c, "Favorite added successfully")
+}
---
+++ user/repository/user_repository.go
@@ -0,0 +1,11 @@
+// user/repository/user_repository.go
+
+package repository
+
+import (
+	"context"
+)
+
+type UserRepository interface {
+	AddFavorite(ctx context.Context, userID int, situationIndex int) error
+}
