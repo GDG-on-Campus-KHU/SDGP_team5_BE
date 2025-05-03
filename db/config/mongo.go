8cd8e589f92276e3c6b613fd4c44c320ecf6bca9
kmin1231 <kmin91143@gmail.com>
2025-05-03 23:35:45 +0900

    ✨ feat(db): #5 initialize MongoDB collection 'AuthToken'

--- db/config/mongo.go
+++ db/config/mongo.go
@@ -19,6 +19,7 @@ var UserCollection *mongo.Collection
 var CountryCollection *mongo.Collection
 var RecordingCollection *mongo.Collection
 var GroupCollection *mongo.Collection
+var AuthTokenCollection *mongo.Collection
 
 func InitMongo() {
 	uri := os.Getenv("MONGO_URI")
@@ -50,4 +51,5 @@ func InitMongo() {
 	CountryCollection = Client.Database("resq").Collection("countries")
 	RecordingCollection = Client.Database("resq").Collection("recordings")
 	GroupCollection = Client.Database("resq").Collection("groups")
+	AuthTokenCollection = Client.Database("resq").Collection("auth_tokens")
 }
