dc68b8c0a57179aa8858c9eb35f93b52634a2809
kmin1231 <kmin91143@gmail.com>
2025-05-05 07:44:31 +0900

    ✨ feat(util): #2 add function to retrieve user and medical info based on requested user id

--- util/get_user.go
+++ util/get_user.go
@@ -5,6 +5,7 @@ package util
 import (
 	"context"
 	"errors"
+	"fmt"
 
 	dbConfig "github.com/GDG-on-Campus-KHU/SDGP_team5_BE/db/config"
 	"github.com/GDG-on-Campus-KHU/SDGP_team5_BE/db/model"
@@ -21,5 +22,29 @@ func GetUserByUserID(ctx context.Context, userID int) (*model.User, error) {
 		return nil, errors.New("user not found")
 	}
 
+	return &user, nil
+}
+
+
+func GetMedicalInfoByID(ctx context.Context, infoID int32) (*model.MedicalInfo, error) {
+	filter := bson.M{"info_id": infoID}
+
+	var medicalInfo model.MedicalInfo
+	err := dbConfig.MedicalInfoCollection.FindOne(ctx, filter).Decode(&medicalInfo)
+	if err != nil {
+		return nil, errors.New("medical info not found")
+	}
+
+	return &medicalInfo, nil
+}
+
+
+func GetUserByIDFromRequest(ctx context.Context, userID int) (*model.User, error) {
+	var user model.User
+
+	err := dbConfig.UserCollection.FindOne(ctx, bson.M{"user_id": userID}).Decode(&user)
+	if err != nil {
+		return nil, fmt.Errorf("user not found: %v", err)
+	}
 	return &user, nil
 }
