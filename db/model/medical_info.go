7e5e3863372e14cbaf741b266bc167d1d52c7e5d
SangYeong Park <tkddud386@gmail.com>
2025-05-06 01:57:58 +0900

    🐛 fix(recording): #39 update 'Medical_info' schema

--- db/model/medical_info.go
+++ db/model/medical_info.go
@@ -10,6 +10,9 @@ import (
 
 type BloodType string
 
+type WeightUnit string
+type HeightUnit string
+
 const (
 	BloodTypeAPlus   BloodType = "A+"
 	BloodTypeAMinus  BloodType = "A-"
@@ -22,6 +25,14 @@ const (
 	BloodTypeOthers  BloodType = "Others"
 )
 
+const (
+	WeightUnitKg    WeightUnit = "kg"
+	WeightUnitPound WeightUnit = "pound"
+	HeightUnitCm    HeightUnit = "cm"
+	HeightUnitInch  HeightUnit = "inch"
+	HeightUnitFeet  HeightUnit = "feet"
+)
+
 type MedicalInfo struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"` // MongoDB ObjectId
 	InfoID     int                `bson:"info_id" json:"info_id"`
@@ -29,7 +40,9 @@ type MedicalInfo struct {
 	Allergy    string             `bson:"allergy" json:"allergy"`
 	Medication string             `bson:"medication" json:"medication"`
 	Height     float64            `bson:"height" json:"height"`
+	HeightUnit HeightUnit         `bson:"height_unit" json:"height_unit"`
 	Weight     float64            `bson:"weight" json:"weight"`
+	WeightUnit WeightUnit         `bson:"weight_unit" json:"weight_unit"`
 	BirthDate  string             `bson:"birth_date" json:"birth_date"`
 	Notes      string             `bson:"notes" json:"notes"`
 	CreatedAt  time.Time          `bson:"created_at,omitempty" json:"created_at,omitempty"` // timestamp
