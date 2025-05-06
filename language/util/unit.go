e071f91b4bfb644091f5954fdca7b587540cff21
kmin1231 <kmin91143@gmail.com>
2025-05-06 23:32:03 +0900

    ✨ feat(util): #2 add function to convert height and weight based on 'CountryCode'

---
+++ language/util/unit.go
@@ -0,0 +1,79 @@
+// language/util/unit.go
+
+package util
+
+import (
+	"github.com/GDG-on-Campus-KHU/SDGP_team5_BE/db/model"
+)
+
+
+const (
+	HeightUnitCm   = model.HeightUnitCm
+	HeightUnitFeet = model.HeightUnitFeet
+	HeightUnitInch = model.HeightUnitInch
+
+	WeightUnitKg    = model.WeightUnitKg
+	WeightUnitPound = model.WeightUnitPound
+)
+
+
+func DefaultHeightUnit(countryCode string) model.HeightUnit {
+	switch countryCode {
+	case "US", "GB":
+		return HeightUnitFeet
+	default:
+		return HeightUnitCm
+	}
+}
+
+func DefaultWeightUnit(countryCode string) model.WeightUnit {
+	switch countryCode {
+	case "US", "GB":
+		return WeightUnitPound
+	default:
+		return WeightUnitKg
+	}
+}
+
+
+func ConvertFeetToCm(feet float64) float64 {
+	return feet * 30.48
+}
+
+func ConvertLbToKg(lb float64) float64 {
+	return lb * 0.453592
+}
+
+
+func ConvertCmToFeet(cm float64) float64 {
+	return cm / 30.48
+}
+
+func ConvertKgToLb(kg float64) float64 {
+	return kg / 0.453592
+}
+
+
+func ConvertHeight(medicalHeight float64, medicalHeightUnit model.HeightUnit, targetHeightUnit model.HeightUnit) (float64, model.HeightUnit) {
+	if medicalHeightUnit == HeightUnitCm && targetHeightUnit != HeightUnitCm {
+		if targetHeightUnit == HeightUnitFeet {
+			medicalHeight = ConvertCmToFeet(medicalHeight)
+		}
+	} else if medicalHeightUnit == HeightUnitFeet && targetHeightUnit == HeightUnitCm {
+		medicalHeight = ConvertFeetToCm(medicalHeight)
+	}
+
+	return medicalHeight, targetHeightUnit
+}
+
+func ConvertWeight(medicalWeight float64, medicalWeightUnit model.WeightUnit, targetWeightUnit model.WeightUnit) (float64, model.WeightUnit) {
+	if medicalWeightUnit == WeightUnitKg && targetWeightUnit != WeightUnitKg {
+		if targetWeightUnit == WeightUnitPound {
+			medicalWeight = ConvertKgToLb(medicalWeight)
+		}
+	} else if medicalWeightUnit == WeightUnitPound && targetWeightUnit == WeightUnitKg {
+		medicalWeight = ConvertLbToKg(medicalWeight)
+	}
+
+	return medicalWeight, targetWeightUnit
+}
